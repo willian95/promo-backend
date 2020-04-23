2081186906ad3228414c4cd305e80a1b37fffa72
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-04-23 19:47:52 -0400

    cambios

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CategoryStoreRequest;
+use Intervention\Image\Facades\Image;
+use Carbon\Carbon;
 use App\Category;
 
 class CategoryController extends Controller
@@ -11,13 +13,24 @@ class CategoryController extends Controller
     
     function store(CategoryStoreRequest $request){
 
-        return response()->json($request->all());
+        try{
+
+            $imageData = $request->get('image');
+            $fileName = Carbon::now()->timestamp . '_' . uniqid() . '.' . explode('/', explode(':', substr($imageData, 0, strpos($imageData, ';')))[1])[1];
+            Image::make($request->get('image'))->save(public_path('images/categories/').$fileName);
+
+        }catch(\Exception $e){
+
+            return response()->json(["success" => false, "msg" => "Hubo un error al cargar la imagen"]);
+
+        }
 
         try{
 
             $category = new Category;
             $category->name = $request->name;
             $category->description = $request->description;
+            $category->image = $fileName;
             $category->save();
             
             return response()->json(["success" => true, "msg" => "Categoría creada"]);
