086b102ce1c4c5faedd47eab0681d53e3c17bf10
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-07-19 21:21:11 -0400

    cambios

--- resources/views/emails/purchaseMail.blade.php
+++ resources/views/emails/purchaseMail.blade.php
@@ -12,15 +12,7 @@
     </thead>
     <tbody>
 
-    @foreach($purchaseProducts as $purchase)
-            <tr>
-                <td>{{ $loop->index + 1 }}</td>
-                <td>{{ $purchase->postProduct->title }}</td>
-                <td>{{ $purchase->amount }}</td>
-                <td>$ {{ $purchase->price }}</td>
-                <td>$ {{ $purchase->price * $purchase->amount }}</td>
-            </tr>
-        @endforeach
+    
         
     </tbody>
 </table>
