a7334eb21e85867192c173db07ba1bfd6aa8fcf2
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-07-19 23:23:37 -0400

    cambios

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Purchase;
+use App\User;
+use App\Post;
 use JWTAuth;
 
 class SaleController extends Controller
@@ -39,6 +41,52 @@ class SaleController extends Controller
         try{
 
             Purchase::where("id", $request->id)->update(["shipping_state" => "entregado"]);
+
+            $purchase = Purchase::where("id", $request->id)->first();
+
+            $post = Post::where("id", $purchase->post_id)->first();
+            $seller = User::where("id", $post->user_id)->first();
+            $buyer = User::where("id", $purchase->user_id)->first();
+            $messageBuyer = "Hola ".$buyer->name."! El vendedor ".$seller->name." indica que te ha entregado los siguientes platos: ";
+            $purchaseProduct = ProductPurchase::with("postProduct")->where("purchase_id", $purchase->id)->get();
+            $to_email = $buyer->email;
+            $to_name = $buyer->name;
+            
+            $data = ["messageMail" => $messageBuyer, "purchaseProducts" => $purchaseProduct, "messageTo" => "buyer"];
+            \Mail::send("emails.deliverMail", $data, function($message) use ($to_name, $to_email) {
+
+                $message->to($to_email, $to_name)->subject("¡Has recibido tus platos!");
+                $message->from( env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
+
+            });
+
+            $messageSeller = "Hola ".$seller->name."! Has entregado a ".$buyer->name." los siguientes platos: ";
+            $purchaseProduct = ProductPurchase::with("postProduct")->where("purchase_id", $purchase->id)->get();
+            $to_email = $seller->email;
+            $to_name = $seller->name;
+            
+            $data = ["messageMail" => $messageSeller, "purchaseProducts" => $purchaseProduct, "messageTo" => "seller"];
+            \Mail::send("emails.deliverMail", $data, function($message) use ($to_name, $to_email) {
+
+                $message->to($to_email, $to_name)->subject("¡Has entregado tus platos!");
+                $message->from( env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
+
+            });
+
+            $messageAdmin = "Hola Admin! El vendedor ".$seller->name." ha entregado a ".$buyer->name." los siguientes platos: ";
+            $purchaseProduct = ProductPurchase::with("postProduct")->where("purchase_id", $purchase->id)->get();
+            $to_email = $seller->email;
+            $to_name = $seller->name;
+            
+            $data = ["messageMail" => $messageAdmin, "purchaseProducts" => $purchaseProduct, "messageTo" => "admin"];
+            \Mail::send("emails.deliverMail", $data, function($message) use ($to_name, $to_email) {
+
+                $message->to($to_email, $to_name)->subject("¡Entrega realizada!");
+                $message->from( env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
+
+            });
+
+
             return response()->json(["success" => true, "msg" => "Articulo entregado, en espera de la confirmación del cliente"]);
 
         }catch(\Exception $e){
