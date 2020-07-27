443e9c91464efade46d41e668337d446d5fa2dbd
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-07-27 11:46:40 -0400

    cambios

--- resources/views/user/landing.blade.php
+++ resources/views/user/landing.blade.php
@@ -83,12 +83,12 @@
 
                     <div class="card" style="padding-bottom: 20px;">
                         <div class="img-product">
-                            <img :src="'{{ url('/images/posts/') }}'+'/'+post.post[0].image" alt="">
+                            <!--<img :src="'{{ url('/images/posts/') }}'+'/'+post.post[0].image" alt="">-->
                         </div>
                         <div class="card-body">
                             <div class="card-image-section">
                                 <p class="text-center">
-                                    <img :src="'{{ url('/') }}'+'/images/users/'+post.post[0].user.image" alt="">
+                                    <!--<img :src="'{{ url('/') }}'+'/images/users/'+post.post[0].user.image" alt="">-->
                                 </p>
                                 <p class="text-center">
                                     <a class="text-center" :href="'{{ url('/') }}'+'/profile'+'/'+post.post[0].user_id">@{{ post.post[0].user.name }}</a>
