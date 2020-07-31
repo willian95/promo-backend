72a52d30efc02609d7f2d6dbc6e68d25a39ed6df
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-07-30 22:52:50 -0400

    cambios

--- resources/views/user/changePassword.blade.php
+++ resources/views/user/changePassword.blade.php
@@ -9,8 +9,8 @@
             <div class="row text-center ftco-heading ftco-animate">
                 <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
                     
-                    <div class="card" style="height: unset;">
-                        <div class="card-body">
+                    <div class="card">
+                        <div class="card-body" style="height: unset;">
 
                             <h3 class="mb-3">Nueva contraseña</h3>
                             
