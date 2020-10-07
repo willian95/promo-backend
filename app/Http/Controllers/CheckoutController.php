800f2874f06e3b8eea0a377f54fd185d8ab7552b
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-10-07 12:22:10 -0400

    cambios

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -6,82 +6,14 @@ use Illuminate\Http\Request;
 use Freshwork\Transbank\WebpayNormal;
 use Freshwork\Transbank\WebpayPatPass;
 use Freshwork\Transbank\RedirectorHelper;
-use Carbon\Carbon;
-use App\CartPurchase;
-use App\CartProductPurchase;
-use Auth;
-use JWTAuth;
 
 class CheckoutController extends Controller
 {
     
-    function storeCart(Request $request){	
-
-		$user = JWTAuth::parseToken()->toUser();
-		CartPurchase::where("user_id", $user->id)->delete();
-		CartProductPurchase::where("user_id", $user->id)->delete();
-			
-		if($request->action == "make-purchase"){
-			
-			$cartPurchase = new CartPurchase;
-			$cartPurchase->user_id = $user->id;
-			$cartPurchase->price = $request->amountToPay;
-			$cartPurchase->total = $request->total;
-			$cartPurchase->payment_type = $request->paymentType;
-			$cartPurchase->post_id = $request->post_id;
-			$cartPurchase->has_delivery = $request->delivery;
-			$cartPurchase->save();
-
-			foreach($request->productPurchases as $productPurchase){
-
-				$cartPurchaseProduct = new CartProductPurchase;
-				$cartPurchaseProduct->post_product_id = $productPurchase["product"]["id"];
-				$cartPurchaseProduct->amount = $productPurchase["amount"];
-				$cartPurchaseProduct->cart_purchase_id = $cartPurchase->id;
-				$cartPurchaseProduct->price = $productPurchase["price"];
-				$cartPurchaseProduct->user_id = $user->id;
-				$cartPurchaseProduct->save();
-
-				
-			}
-
-			return response()->json(["success" => true]);
-
-		}else{
-
-			//session_start();
-			//Session::put('purchase_id',$request->purchase_id);
-			//Session::put('purchase_price',$request->amountToPay);
-			
-			//$_SESSION["purchase_id"]=$request->purchase_id;
-			//$_SESSION["purchase_price"] = $request->amountToPay;
-
-			session(["purchase_id", $request->purchase_id]);
-			session(["purchase_price", $request->amountToPay]);
-
-			return response()->json(["success" => true]);
-
-		}
-
-	}
-	
-	public function initTransaction(WebpayNormal $webpayNormal)
+    public function initTransaction(WebpayNormal $webpayNormal)
 	{
 		
-		$user = JWTAuth::parseToken()->toUser();
-        $cart = CartPurchase::where("user_id", $user->id)->first();
-		$order = Carbon::now()->timestamp.uniqid();
-
-		$price = 0;
-		if(session("purchase_price") != null){
-			$price = session("purchase_price");
-			
-		}else{
-			$price = $cart->price;
-		}
-
-		$webpayNormal->addTransactionDetail(intval($price) + 1, $order);  
-		//$response = $webpayNormal->initTransaction(route('checkout.webpay.response'), route('checkout.webpay.finish'), null, 'TR_NORMAL_WS', null, null);
+		$webpayNormal->addTransactionDetail(1500, 'order-' . rand(1000, 9999));  
 		$response = $webpayNormal->initTransaction(route('checkout.webpay.response'), route('checkout.webpay.finish'), null, 'TR_NORMAL_WS', null, null); 
 		// Probablemente también quieras crear una orden o transacción en tu base de datos y guardar el token ahí.
 
@@ -93,19 +25,20 @@ class CheckoutController extends Controller
 		$result = $webpayPatPass->getTransactionResult();  
 		session(['response' => $result]);  
 
-		$webpayPatPass->acknowledgeTransaction();
+			$webpayPatPass->acknowledgeTransaction();
 
 		// Revisar si la transacción fue exitosa ($result->detailOutput->responseCode === 0) o fallida para guardar ese resultado en tu base de datos. 
 		//return RedirectorHelper::redirectBackNormal($result->urlRedirection);
 		return RedirectorHelper::redirectBackNormal($result->urlRedirection);  
 	}
 
-	public function finish(Request $request)  
+	public function finish()  
 	{
+
+		
 
 		dd($_POST, session('response'));  
 		// Acá buscar la transacción en tu base de datos y ver si fue exitosa o fallida, para mostrar el mensaje de gracias o de error según corresponda
 	}
-
 
 }
